7c49ff745c4d3d2d9bcfe236d0a11393d0111b41
m19e <nexus.me7290@gmail.com>
2022-07-21 21:46:23 +0900

    Test twind activation

--- routes/index.tsx
+++ routes/index.tsx
@@ -8,7 +8,9 @@ export default function Home() {
     return (
         <div class={tw`p-4 mx-auto max-w-screen-md`}>
             <img src="/logo.svg" height="100px" alt="the fresh logo: a sliced lemon dripping with juice" />
-            <p class={tw`my-6`}>Welcome to `fresh`. Try updating this message in the ./routes/index.tsx file, and refresh.</p>
+            <p class={tw`my-6`}>
+                Welcome to <span class={tw`text-yellow-500 font-black`}>`fresh`</span>. Try updating this message in the ./routes/index.tsx file, and refresh.
+            </p>
             <Counter start={3} />
         </div>
     );
